e798ed7d5d0add5d1279a57e1d4814ac5af98552
Neha Tevathia <nehatevathia@Nehas-MacBook-Pro.local>
2019-03-22 17:51:42 -0700

    updated

--- user_definition.py
+++ user_definition.py
@@ -1,3 +1,14 @@
 orig_coord = '37.7909,-122.3925'
 dest_coord = '37.7765,-122.4506'
 output_file_name = 'output.txt'
+
+ec2_address = "ec2-34-217-102-137.us-west-2.compute.amazonaws.com"
+user = "ec2-user"
+key_file = "/Desktop/aws_neha_oregon.pem"
+git_repo_owner = "MSDS698"
+git_repo_name = "MSDS603"
+git_user_id = "ntevathia"
+orig_coord = '37.7909,-122.3925'
+dest_coord = '37.7765,-122.4506'
+output_file_name = 'output.txt'
+
