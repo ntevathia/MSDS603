510a8890926214c13904f49d278cf5554e3c3a81
Neha Tevathia <nehatevathia@Nehas-MacBook-Pro.local>
2019-03-24 11:51:06 -0700

    modified user_def

--- user_definition.py
+++ user_definition.py
@@ -1,7 +1,3 @@
-orig_coord = '37.7909,-122.3925'
-dest_coord = '37.7765,-122.4506'
-output_file_name = 'output.txt'
-
 ec2_address = "ec2-34-217-102-137.us-west-2.compute.amazonaws.com"
 user = "ec2-user"
 key_file = "/Desktop/aws_neha_oregon.pem"
@@ -11,4 +7,3 @@ git_user_id = "ntevathia"
 orig_coord = '37.7909,-122.3925'
 dest_coord = '37.7765,-122.4506'
 output_file_name = 'output.txt'
-
